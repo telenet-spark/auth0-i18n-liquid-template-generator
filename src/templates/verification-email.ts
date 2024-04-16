00e70057a1d32bd5da71a7587d5d1a69dc2064eb
Robbe Dekien <robbe@bothrs.com>
2024-04-16 15:45:59 +0200

    feat: verification email

--- src/templates/verification-email.ts
+++ src/templates/verification-email.ts
@@ -1,63 +1,64 @@
 import { localizeMessage } from "../scripts";
 
 const html = `
-<html>
-   <head>
-      <style type="text/css">
-         .ExternalClass,.ExternalClass div,.ExternalClass font,.ExternalClass p,.ExternalClass span,.ExternalClass td,img {line-height: 100%;}#outlook a {padding: 0;}.ExternalClass,.ReadMsgBody {width: 100%;}a,blockquote,body,li,p,table,td {-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;}table,td {mso-table-lspace: 0;mso-table-rspace: 0;}img {-ms-interpolation-mode: bicubic;border: 0;height: auto;outline: 0;text-decoration: none;}table {border-collapse: collapse !important;}#bodyCell,#bodyTable,body {height: 100% !important;margin: 0;padding: 0;font-family: ProximaNova, sans-serif;}#bodyCell {padding: 20px;}#bodyTable {width: 600px;}@font-face {font-family: ProximaNova;src: url(https://cdn.auth0.com/fonts/proxima-nova/proximanova-regular-webfont-webfont.eot);src: url(https://cdn.auth0.com/fonts/proxima-nova/proximanova-regular-webfont-webfont.eot?#iefix)format("embedded-opentype"),url(https://cdn.auth0.com/fonts/proxima-nova/proximanova-regular-webfont-webfont.woff) format("woff");font-weight: 400;font-style: normal;}@font-face {font-family: ProximaNova;src: url(https://cdn.auth0.com/fonts/proxima-nova/proximanova-semibold-webfont-webfont.eot);src: url(https://cdn.auth0.com/fonts/proxima-nova/proximanova-semibold-webfont-webfont.eot?#iefix)format("embedded-opentype"),url(https://cdn.auth0.com/fonts/proxima-nova/proximanova-semibold-webfont-webfont.woff) format("woff");font-weight: 600;font-style: normal;}@media only screen and (max-width: 480px) {#bodyTable,body {width: 100% !important;}a,blockquote,body,li,p,table,td {-webkit-text-size-adjust: none !important;}body {min-width: 100% !important;}#bodyTable {max-width: 600px !important;}#signIn {max-width: 280px !important;}}
-      </style>
-   </head>
-   <body>
-      <center>
-         <table
-            style='width: 600px;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;margin: 0;padding: 0;font-family: "ProximaNova", sans-serif;border-collapse: collapse !important;height: 100% !important;'
-            align="center"
-            border="0"
-            cellpadding="0"
-            cellspacing="0"
-            height="100%"
-            width="100%"
-            id="bodyTable"
-            >
-            <tr>
-               <td
-                  align="center"
-                  valign="top"
-                  id="bodyCell"
-                  style='-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;margin: 0;padding: 20px;font-family: "ProximaNova", sans-serif;height: 100% !important;'
-                  >
-                  <div class="main">
-                     <p
-                        style="text-align: center;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%; margin-bottom: 30px;">
-                        <img
-                           src="https://cdn.auth0.com/styleguide/2.0.9/lib/logos/img/badge.png"
-                           width="50"
-                           alt="Your logo goes here"
-                           style="-ms-interpolation-mode: bicubic;border: 0;height: auto;line-height: 100%;outline: none;text-decoration: none;"
-                           />
-                     </p>
-                     <h1>${localizeMessage("welcome")}</h1>
-                     <p>${localizeMessage("thankYou")}</p>
-                     <p><a href="{{ url }}">${localizeMessage("confirm")}</a></p>
-                     <p>
-                        ${localizeMessage("needHelp")}
-                     </p>
-                     <br />
-                     ${localizeMessage("regards")}
-                     <br />
-                     <strong>{{ application.name }}</strong>
-                     <br /><br />
-                     <hr style="border: 2px solid #EAEEF3; border-bottom: 0; margin: 20px 0;" />
-                     <p style="text-align: center;color: #A9B3BC;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;">
-                        ${localizeMessage("footer")}
-                     </p>
-                  </div>
-               </td>
-            </tr>
-         </table>
-      </center>
-   </body>
-</html>
+<table style="background-color: #12292F; border: none; width: 100%;" cellpadding="0" cellspacing="0">
+    <tbody>
+        <tr>
+            <td>
+                <div
+                    style="border: none; box-sizing: border-box; max-width: 640px; overflow: hidden; width: 100%; margin: 20px auto; padding: 0px 20px;">
+                    <span class="font" style="font-family:verdana, sans-serif"><img src="https://uploads-ssl.webflow.com/659e550456aad682f3ccbdf3/65b123a1cf55251bd879ebe1_logo-color.png" height="22" style="height: 22px;"><br></span>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div
+                    style="border: none; box-sizing: border-box; font-family: Lato, Helvetica, 'Helvetica Neue', Arial, 'sans-serif'; max-width: 640px; overflow: hidden; width: 100%; margin: 10px auto; margin-top: 10px; padding: 0px 20px;">
+                    <h1 style="font-size: 32px; font-weight: 600; line-height: 38px; color: white; margin-bottom: 0px;">
+                        <span class="font" style="font-family:verdana, sans-serif">${localizeMessage("email-verification.title")}<br></span>
+                    </h1>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div
+                    style="border: none; box-sizing: border-box; max-width: 640px; padding: 0px 20px; width: 100%; margin: 0px auto;">
+                    <div
+                        style="border: none; box-sizing: border-box; width: 100%; height: 300px; background-image: url('https://uploads-ssl.webflow.com/659e550456aad682f3ccbdf3/65b11918c055ac2c7f51d3b0_car-charging-talking.png'); background-repeat: no-repeat; background-position: center; background-size: cover;">
+                        <span class="font" style="font-family:verdana, sans-serif"><br></span>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        <tr style="background-color: #fff;">
+            <td>
+                <div
+                    style="border: none; box-sizing: border-box; width: 100%; max-width: 640px; padding: 20px 20px; margin: 0px auto; color: #291D18;">
+                    <h3 style="font-size: 20px; font-weight: 600; line-height: 20px;">
+                        <span class="font" style="font-family:verdana, sans-serif">${localizeMessage("email-verification.content.title")}<br></span>
+                    </h3>
+                    <p style="line-height: 24px; margin-bottom: 30px;">
+                        <span class="size" style="font-size: 14px; line-height: 24px; margin-bottom: 30px;"><span class="font" style="font-family:verdana, sans-serif">${localizeMessage("email-verification.content.description")}</span></span><span class="font" style="font-family:verdana, sans-serif"><br></span>
+                    </p><a href="{{ url }}"
+                        style="font-size: 14px; color: #DCF730; cursor: pointer; line-height: normal; text-align: center; text-decoration: none; cursor: pointer; padding: 14px 18px; background-color: #12292F; border-radius: 6px; display: inline-block; margin-bottom: 10px;"><span class="font" style="font-family:verdana, sans-serif">${localizeMessage("email-verification.content.submit")}</span></a><span class="font" style="font-family:verdana, sans-serif"> </span>
+                </div>
+            </td>
+        </tr>
+        <tr style="background-color: #fff;">
+            <td>
+                <div
+                    style="border: none; box-sizing: border-box; width: 100%; max-width: 640px; padding: 20px 20px; margin: 0px auto; color: #291D18; padding-top: 0px">
+                    <hr>
+                    <p style="line-height: 24px; margin-bottom: 30px; margin-top: 50px">
+                        <span class="size" style="font-size: 14px; line-height: 20px; margin-bottom: 30px;"><span class="font" style="font-family:verdana, sans-serif">© Blossom BV • Maatschappelijke zetel • Liersesteenweg 4, 2800 Mechelen.<br>BTW BE0473.416.418 • RPR Antwerpen, afdeling Mechelen.<br /><a style="color: #291D18" href="https://www.blossom.be/privacy-policy">Privacy statement</a> | <a style="color: #291D18" href="www.blossom.be/terms-and-conditions">Algemene voorwaarden</a></span></span><span class="font" style="font-family:verdana, sans-serif"><br></span>
+                    </p>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
 `;
 
 export default html;
